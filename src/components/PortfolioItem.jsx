a585f207b9f11e9ad7a6c9880327d44e16952c05
simskberna <simskberna@gmail.com>
2023-08-30 21:04:25 +0300

    small fixes applied

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -20,7 +20,7 @@ const PortfolioItem = ({title,imgUrl,stack,link,githubLink}) => {
                 <p className='flex flex-wrap gap-2 flex-row items-center justify-start
                     text-xs md:text-sm dark:text-white'> 
                         {stack.map(item => (
-                            <span className='inline-block px-2
+                            <span key={title} className='inline-block px-2
                             py-1 font-semibold border-2 border-stone-900
                             rounded-md dark:border-white'>
                                 {item}
