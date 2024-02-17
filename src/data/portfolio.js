6a7a107b2f90e98912ad98001ab91025fe7587a9
simskberna <simskberna@gmail.com>
2024-02-17 22:46:01 +0300

    new project added

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,26 +1,34 @@
 export default [
     {
-        title: 'Ecommerce Website',
+        title: 'Movies Web App',
+        imgUrl: '/assets/movies-app.png',
+        stack: ['Java','Spring Boot','JavaScript', 'React.js','Bootstrap'], 
+        githubLink: 'https://github.com/simskberna/java-movies-api-backend.git',
+        link: 'https://react-movies-app-frontend.vercel.app/',
+    },
+
+    {
+        title: 'Ecommerce Web App',
         imgUrl: '/assets/home.png',
         stack: ['React.js','Tailwind','Nodejs/Express','Google Cloud VM'],
         githubLink: 'https://github.com/simskberna/web-app-fullstack-project',
         link: 'https://homeappliances.vercel.app/',
-    },
-    {
-        title: 'Tenzies Game',
-        imgUrl: '/assets/reactlogo.png',
-        stack: ['CSS', 'JavaScript', 'React.js'],
-        githubLink: 'https://github.com/simskberna/tenziesreactproject',
-        link: 'http://tenzies.s3-website-us-east-1.amazonaws.com/',
-    },  
+    }, 
     
     {
         title: 'Rick And Morty App',
         imgUrl: '/assets/rickandmortylogo.png',
-        stack: ['Tailwind CSS', 'JavaScript', 'React.js'],
+        stack: ['JavaScript', 'React.js','Tailwind CSS',],
         githubLink : 'https://github.com/simskberna/rickandmorty',
         link: 'http://rickandmortybucket2023.s3-website-us-west-1.amazonaws.com/',
     }, 
+    {
+        title: 'Tenzies Game',
+        imgUrl: '/assets/reactlogo.png',
+        stack: [ 'JavaScript', 'React.js','CSS'],
+        githubLink: 'https://github.com/simskberna/tenziesreactproject',
+        link: 'http://tenzies.s3-website-us-east-1.amazonaws.com/',
+    },  
     {
         title: 'Shopping List App',
         imgUrl: '/assets/vuelogo.png',
